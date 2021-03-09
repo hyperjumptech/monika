9c6395473fccafbf607ce2b6aedc97c389948fb0
Nico Prananta <nicnocquee@users.noreply.github.com>
2021-03-09 16:04:29 +0100

    update landing page

--- docs/src/siteConfig.js
+++ docs/src/siteConfig.js
@@ -1,13 +1,13 @@
 // List of projects/orgs using your project for the users page.
 export const siteConfig = {
   editUrl: 'https://github.com/hyperjumptech/monika/edit/master/docs/src/pages',
-  copyright: `Copyright © ${new Date().getFullYear()} Tanner Linsley. All Rights Reserved.`,
+  copyright: `Copyright © ${new Date().getFullYear()} Hyperjump Tech. All Rights Reserved.`,
   repoUrl: 'https://github.com/hyperjumptech/monika',
-  // algolia: {
-  //   appId: 'BH4D9OD16A',
-  //   apiKey: 'c97369bb0f67e99c8acf34bfd732cca7',
-  //   indexName: 'tanstack_react-query', // algoliaOptions: {
-  //   //   facetFilters: ['version:VERSION'],
-  //   // },
-  // },
+  algolia: {
+    appId: '',
+    apiKey: '',
+    indexName: '', // algoliaOptions: {
+    //   facetFilters: ['version:VERSION'],
+    // },
+  },
 }
