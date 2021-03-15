26933887725e4cccbe71dc054515da39211e6b15
Hari Cahya Nugraha <haricnugraha@gmail.com>
2021-03-15 16:50:22 +0700

    Chore: Change example, test, and documentation domain to example.com

--- test/utils/is-valid-url.test.ts
+++ test/utils/is-valid-url.test.ts
@@ -3,19 +3,19 @@ import { isValidURL } from './../../src/utils/is-valid-url'
 
 describe('check if URL is valid', () => {
   it('should return true if URL using http protocol', () => {
-    const valid = isValidURL('http://www.google.com')
+    const valid = isValidURL('http://www.example.com')
     expect(valid).to.be.a('boolean')
     expect(valid).to.be.equals(true)
   })
 
   it('should return true if URL using https protocol', () => {
-    const valid = isValidURL('https://www.google.com')
+    const valid = isValidURL('https://www.example.com')
     expect(valid).to.be.a('boolean')
     expect(valid).to.be.equals(true)
   })
 
   it('should return false if not using http or https protocol', () => {
-    const valid = isValidURL('www.google.com')
+    const valid = isValidURL('www.example.com')
     expect(valid).to.be.a('boolean')
     expect(valid).to.be.equals(false)
   })
