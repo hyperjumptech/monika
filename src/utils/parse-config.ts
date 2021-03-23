825c595d741c506abb8f357e3c7f0aa19e764b44
Denny Pradipta <denny@hyperjump.tech>
2021-03-23 14:23:53 +0700

    fix: improve error logs (#76)

--- src/utils/parse-config.ts
+++ src/utils/parse-config.ts
@@ -1,19 +1,31 @@
-import { readFileSync } from 'fs'
-import { Config } from '../interfaces/config'
+import { Config } from './../interfaces/config'
+import { readFile } from 'fs'
+import { promisify } from 'util'
 
 export const parseConfig = async (configPath: string) => {
+  // Read file from configPath
   try {
     // Read file from configPath
-    const config: string = readFileSync(configPath).toString()
+    const readFileAsync = promisify(readFile)
+    const config: Buffer = await readFileAsync(configPath)
 
     // Parse the content
-    const output: Config = JSON.parse(config)
+    const configString: string = await config.toString()
+    const output: Config = await JSON.parse(configString)
 
     // Return the output as string
     return output
   } catch (error) {
-    throw new Error(
-      'Config file not found! Copy example config from https://raw.githubusercontent.com/hyperjumptech/monika/main/config.example.json'
-    )
+    if (error.code === 'ENOENT' && error.path === configPath) {
+      throw new Error(
+        'JSON configuration file not found! Copy example config from https://raw.githubusercontent.com/hyperjumptech/monika/main/config.example.json'
+      )
+    }
+
+    if (error.name === 'SyntaxError') {
+      throw new Error('JSON configuration file is in invalid JSON format!')
+    }
+
+    throw new Error(error.message)
   }
 }
