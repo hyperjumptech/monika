b8b6b71fd3d43240a86716a781bd575943b81f9f
muslim <muslim.ilmiawan@gmail.com>
2021-03-30 19:00:44 +0700

    fix(config): create new config object instead of modifying existing object

--- src/utils/validate-config.ts
+++ src/utils/validate-config.ts
@@ -35,121 +35,80 @@ import {
   WhatsappData,
 } from './../interfaces/data'
 import { Config } from '../interfaces/config'
-import { Probe } from '../interfaces/probe'
 import { RequestConfig } from './../interfaces/request'
 import { isValidURL } from './is-valid-url'
 import chalk from 'chalk'
 import boxen from 'boxen'
+
+const setInvalidResponse = (message: string) => ({
+  valid: false,
+  message: message,
+  config: { probes: [] },
+})
 
 // Validations messages
 const VALID_CONFIG = {
   valid: true,
   message: '',
 }
-const NO_NOTIFICATIONS = {
-  valid: false,
-  message: `Notifications config has not been set. We will not be able to notify you when INCIDENT happened!\nPlease refer to Monika documentation for setting the notifications config at https://hyperjumptech.github.io/monika/guides/notifications.`,
-}
-const NO_PROBES = {
-  valid: false,
-  message: 'Probes object does not exists or has length lower than 1!',
-}
+const NO_NOTIFICATIONS = setInvalidResponse(
+  `Notifications config has not been set. We will not be able to notify you when INCIDENT happened!\nPlease refer to Monika documentation for setting the notifications config at https://hyperjumptech.github.io/monika/guides/notifications.`
+)
+const NO_PROBES = setInvalidResponse(
+  'Probes object does not exists or has length lower than 1!'
+)
 
 // Notification
-const NOTIFICATION_NO_RECIPIENTS = {
-  valid: false,
-  message: 'Recipients does not exists or has length lower than 1!',
-}
-const NOTIFICATION_INVALID_TYPE = {
-  valid: false,
-  message: 'Notifications type is not allowed',
-}
+const NOTIFICATION_NO_RECIPIENTS = setInvalidResponse(
+  'Recipients does not exists or has length lower than 1!'
+)
+const NOTIFICATION_INVALID_TYPE = setInvalidResponse(
+  'Notifications type is not allowed'
+)
 
-const PROBE_NO_REQUESTS = {
-  valid: false,
-  message: 'Probe requests does not exists or has length lower than 1!',
-}
-const PROBE_REQUEST_INVALID_URL = {
-  valid: false,
-  message: 'Probe request URL should start with http:// or https://',
-}
-const PROBE_REQUEST_INVALID_METHOD = {
-  valid: false,
-  message: 'Probe request method should be GET or POST only',
-}
-const PROBE_ALERT_INVALID = {
-  valid: false,
-  message:
-    "Probe alert should be 'status-not-2xx' or 'response-time-greater-than-<number>-(m)s",
-}
-const PROBE_DUPLICATE_ID = {
-  valid: false,
-  message: 'Probe should have unique id',
-}
+const PROBE_NO_REQUESTS = setInvalidResponse(
+  'Probe requests does not exists or has length lower than 1!'
+)
+const PROBE_REQUEST_INVALID_URL = setInvalidResponse(
+  'Probe request URL should start with http:// or https://'
+)
+const PROBE_REQUEST_INVALID_METHOD = setInvalidResponse(
+  'Probe request method should be GET or POST only'
+)
+const PROBE_ALERT_INVALID = setInvalidResponse(
+  `Probe alert should be 'status-not-2xx' or 'response-time-greater-than-<number>-(m)s`
+)
+const PROBE_DUPLICATE_ID = setInvalidResponse('Probe should have unique id')
 
 // SMTP
-const SMTP_NO_HOSTNAME = {
-  valid: false,
-  message: 'Hostname not found',
-}
-const SMTP_NO_PORT = {
-  valid: false,
-  message: 'Port not found',
-}
-const SMTP_NO_USERNAME = {
-  valid: false,
-  message: 'Username not found',
-}
-const SMTP_NO_PASSWORD = {
-  valid: false,
-  message: 'Password not found',
-}
+const SMTP_NO_HOSTNAME = setInvalidResponse('Hostname not found')
+const SMTP_NO_PORT = setInvalidResponse('Port not found')
+const SMTP_NO_USERNAME = setInvalidResponse('Username not found')
+const SMTP_NO_PASSWORD = setInvalidResponse('Password not found')
 
 // Mailgun
-const MAILGUN_NO_APIKEY = {
-  valid: false,
-  message: 'API key not found',
-}
-const MAILGUN_NO_DOMAIN = {
-  valid: false,
-  message: 'Domain not found',
-}
+const MAILGUN_NO_APIKEY = setInvalidResponse('API key not found')
+const MAILGUN_NO_DOMAIN = setInvalidResponse('Domain not found')
 
 // Sendgrid
-const SENDGRID_NO_APIKEY = {
-  valid: false,
-  message: 'API key not found',
-}
+const SENDGRID_NO_APIKEY = setInvalidResponse('API key not found')
 
 // Webhook
-const WEBHOOK_NO_URL = {
-  valid: false,
-  message: 'URL not found',
-}
+const WEBHOOK_NO_URL = setInvalidResponse('URL not found')
 
 // Whatsapp
-const WHATSAPP_NO_URL = {
-  valid: false,
-  message: 'URL not found',
-}
-
-// Whatsapp
-const WHATSAPP_NO_USERNAME = {
-  valid: false,
-  message: 'Username not found',
-}
-
-// Whatsapp
-const WHATSAPP_NO_PASSWORD = {
-  valid: false,
-  message: 'Password not found',
-}
+const WHATSAPP_NO_URL = setInvalidResponse('Whatsapp URL not found')
+const WHATSAPP_NO_USERNAME = setInvalidResponse('Whatsapp Username not found')
+const WHATSAPP_NO_PASSWORD = setInvalidResponse('Whatsapp Password not found')
 
 export const validateConfig = async (configuration: Config) => {
   const data = configuration
+  const { notifications, probes, interval } = configuration
+  const resultNotif = []
+  const resultProbes = []
 
   // Validate Notifications
-  if (!data.notifications || (data?.notifications?.length ?? 0) === 0) {
+  if (!notifications || (notifications.length ?? 0) === 0) {
     warn(
       boxen(chalk.yellow(NO_NOTIFICATIONS.message), {
         padding: 1,
@@ -163,10 +122,10 @@ export const validateConfig = async (configuration: Config) => {
   // Validate probes
   if ((data?.probes?.length ?? 0) === 0) return NO_PROBES
 
-  if (data.notifications && data.notifications.length > 0) {
+  if (notifications && notifications.length > 0) {
     // Check notifications properties
-    for (const notification of data.notifications) {
-      const { type, data } = notification as Notification
+    for (const notification of notifications) {
+      const { id, type, data } = notification as Notification
 
       // Check if type equals to mailgun, smtp, or sendgrid, and has no recipients
       if (
@@ -176,7 +135,7 @@ export const validateConfig = async (configuration: Config) => {
         return NOTIFICATION_NO_RECIPIENTS
 
       switch (type) {
-        case 'smtp':
+        case 'smtp': {
           if (!(data as SMTPData).hostname) return SMTP_NO_HOSTNAME
 
           if (!(data as SMTPData).port) return SMTP_NO_PORT
@@ -185,35 +144,108 @@ export const validateConfig = async (configuration: Config) => {
 
           if (!(data as SMTPData).password) return SMTP_NO_PASSWORD
 
+          const {
+            hostname,
+            port,
+            username,
+            password,
+            recipients,
+          } = data as SMTPData
+          resultNotif.push({
+            id: id,
+            type: type,
+            data: {
+              recipients,
+              hostname,
+              port,
+              username,
+              password,
+            } as SMTPData,
+          } as Notification)
+
           break
-        case 'mailgun':
+        }
+        case 'mailgun': {
           if (!(data as MailgunData).apiKey) return MAILGUN_NO_APIKEY
 
           if (!(data as MailgunData).domain) return MAILGUN_NO_DOMAIN
 
+          const { apiKey, domain, recipients } = data as MailgunData
+          resultNotif.push({
+            id: id,
+            type: type,
+            data: {
+              recipients,
+              apiKey,
+              domain,
+            } as MailgunData,
+          } as Notification)
+
           break
-        case 'sendgrid':
+        }
+        case 'sendgrid': {
           if (!(data as SendgridData).apiKey) return SENDGRID_NO_APIKEY
+          const { apiKey, recipients } = data as SendgridData
+          resultNotif.push({
+            id: id,
+            type: type,
+            data: {
+              recipients,
+              apiKey,
+            } as SendgridData,
+          } as Notification)
 
           break
-        case 'webhook':
+        }
+        case 'webhook': {
           if (!(data as WebhookData).url) return WEBHOOK_NO_URL
-          break
 
-        case 'slack':
+          const { url } = data as WebhookData
+          resultNotif.push({
+            id: id,
+            type: type,
+            data: {
+              url,
+            } as WebhookData,
+          } as Notification)
+
+          break
+        }
+        case 'slack': {
           if (!(data as WebhookData).url) return WEBHOOK_NO_URL
 
-          break
+          const { url } = data as WebhookData
+          resultNotif.push({
+            id: id,
+            type: type,
+            data: {
+              url,
+            } as WebhookData,
+          } as Notification)
 
-        case 'whatsapp':
+          break
+        }
+        case 'whatsapp': {
           if (!(data as WhatsappData).url) return WHATSAPP_NO_URL
 
           if (!(data as WhatsappData).username) return WHATSAPP_NO_USERNAME
 
           if (!(data as WhatsappData).password) return WHATSAPP_NO_PASSWORD
 
-          break
+          const { url, username, password, recipients } = data as WhatsappData
+          resultNotif.push({
+            id: id,
+            type: type,
+            data: {
+              recipients,
+              url,
+              username,
+              password,
+            } as WhatsappData,
+          } as Notification)
 
+          break
+        }
         default:
           return NOTIFICATION_INVALID_TYPE
       }
@@ -221,38 +253,45 @@ export const validateConfig = async (configuration: Config) => {
   }
 
   let i = 1
+  const defaultThreshold = 5
   // Check probes properties
-  for (const probe of data.probes) {
-    const { requests, incidentThreshold, recoveryThreshold } = probe as Probe
+  for (const probe of probes) {
+    const { requests } = probe
+    let { id, name, incidentThreshold, recoveryThreshold, alerts } = probe
 
-    if (!probe.id) {
-      probe.id = `probe_${i++}`
+    if (!id) {
+      id = `${i}`
     }
 
-    if (!probe.name) {
-      probe.name = `monika_probe_${i++}`
+    if (!name) {
+      name = `monika_${i}`
       warn(
-        `Warning: Probe ${probe.id} has no name defined. Using the default name started by monika`
+        `Warning: Probe ${id} has no name defined. Using the default name started by monika`
+      )
+    }
+
+    if (!incidentThreshold) {
+      incidentThreshold = defaultThreshold
+      warn(
+        `Warning: Probe ${id} has no incidentThreshold configuration defined. Using the default threshold: 5`
+      )
+    }
+
+    if (!recoveryThreshold) {
+      recoveryThreshold = defaultThreshold
+      warn(
+        `Warning: Probe ${id} has no recoveryThreshold configuration defined. Using the default threshold: 5`
       )
     }
 
     if ((requests?.length ?? 0) === 0) return PROBE_NO_REQUESTS
 
-    if ((probe.alerts?.length ?? 0) === 0) {
-      probe.alerts = ['status-not-2xx', 'response-time-greater-than-2-s']
+    if ((alerts?.length ?? 0) === 0) {
+      alerts = ['status-not-2xx', 'response-time-greater-than-2-s']
       warn(
-        `Warning: Probe ${probe.id} has no Alerts configuration defined. Using the default status-not-2xx and response-time-greater-than-2-s`
+        `Warning: Probe ${id} has no Alerts configuration defined. Using the default status-not-2xx and response-time-greater-than-2-s`
       )
     }
-
-    if (!incidentThreshold)
-      warn(
-        `Warning: Probe ${probe.id} has no incidentThreshold configuration defined. Using the default threshold: 5`
-      )
-    if (!recoveryThreshold)
-      warn(
-        `Warning: Probe ${probe.id} has no recoveryThreshold configuration defined. Using the default threshold: 5`
-      )
 
     // Check probe request properties
     for (const request of requests) {
@@ -268,19 +307,37 @@ export const validateConfig = async (configuration: Config) => {
         return PROBE_REQUEST_INVALID_METHOD
 
       // Check probe alert properties
-      for (const alert of probe.alerts) {
+      for (const alert of alerts) {
         const check = getCheckResponseFn(alert)
         if (!check) {
           return PROBE_ALERT_INVALID
         }
       }
     }
+
+    resultProbes.push({
+      id,
+      name,
+      requests,
+      incidentThreshold,
+      recoveryThreshold,
+      alerts,
+    })
+
+    i++
   }
 
   // Check duplicate probe id
-  const probeIds = data.probes.map((probe) => probe.id)
+  const probeIds = resultProbes.map((probe) => probe.id)
   const uniqueProbeIds = new Set(probeIds)
-  if (uniqueProbeIds.size !== data.probes.length) return PROBE_DUPLICATE_ID
+  if (uniqueProbeIds.size !== resultProbes.length) return PROBE_DUPLICATE_ID
 
-  return VALID_CONFIG
+  return {
+    ...VALID_CONFIG,
+    config: {
+      interval: interval,
+      notifications: resultNotif,
+      probes: resultProbes,
+    },
+  }
 }
