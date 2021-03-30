405914ec192c733e5e280665acda7b49e0572b82
Ari Awan <ariawan@hyperjump.tech>
2021-03-30 10:00:18 +0700

    Fix broken URL to edit on github (#111)
    right now all edit URL is pointing to master branch, since there is no master anymore, so its broken

--- docs/src/siteConfig.js
+++ docs/src/siteConfig.js
@@ -1,6 +1,6 @@
 // List of projects/orgs using your project for the users page.
 export const siteConfig = {
-  editUrl: 'https://github.com/hyperjumptech/monika/edit/master/docs/src/pages',
+  editUrl: 'https://github.com/hyperjumptech/monika/edit/main/docs/src/pages',
   copyright: `Copyright © ${new Date().getFullYear()} Hyperjump Tech. All Rights Reserved.`,
   repoUrl: 'https://github.com/hyperjumptech/monika',
   algolia: {
