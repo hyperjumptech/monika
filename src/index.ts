f224b2fa35a3ff2842514220294097003f28e086
Armedi <armedi@outlook.com>
2021-04-26 15:45:18 +0700

    report to monika-hq and update config periodically  (#128)
    * feat: report to monika-hq and update config periodically
    
    * refactor: config update mechanism
    
    * fix: wrong report interval default
    
    * fix: report flow interval running on test mode
    
    * refactor: separate startup message and report interval to their own method and function
    
    * feat: read from history database and send in report to monika hq periodically
    
    * feat: gzip data for probe history report
    
    * fix: set report content-type to application/json
    
    Co-authored-by: Nico Prananta <nicnocquee@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,6 @@
  * SOFTWARE.                                                                      *
  **********************************************************************************/
 
-/* eslint-disable no-console */
 import { Command, flags } from '@oclif/command'
 import cli from 'cli-ux'
 import chalk from 'chalk'
@@ -30,7 +29,8 @@ import boxen from 'boxen'
 import open from 'open'
 import fs from 'fs'
 import { MailData, MailgunData, SMTPData, WebhookData } from './interfaces/data'
-import { looper } from './components/looper'
+import { Config } from './interfaces/config'
+import { loopProbes, loopReport } from './components/looper'
 import { printAllLogs } from './components/logger'
 import { log } from './utils/pino'
 import {
@@ -39,7 +39,11 @@ import {
   flushAllLogs,
 } from './components/logger/history'
 import { notificationChecker } from './components/notification/checker'
-import { getConfig, setupConfigFromFile } from './components/config'
+import {
+  getConfig,
+  getConfigIterator,
+  setupConfigFromFile,
+} from './components/config'
 
 function getDefaultConfig() {
   const filesArray = fs.readdirSync('./')
@@ -120,94 +124,129 @@ class Monika extends Command {
       return
     }
 
-    // Read the config
-    const file = flags.config
-
     try {
-      await setupConfigFromFile(file)
-      const config = getConfig()
+      await setupConfigFromFile(flags.config)
 
-      const { notifications, probes } = config
-
-      // warn if config is empty
-      if ((notifications?.length ?? 0) === 0) {
-        const NO_NOTIFICATIONS_MESSAGE = `Notifications has not been set. We will not be able to notify you when an INCIDENT occurs!\nPlease refer to the Monika documentations on how to configure notifications at https://hyperjumptech.github.io/monika/guides/notifications.`
-
-        log.warn(
-          boxen(chalk.yellow(NO_NOTIFICATIONS_MESSAGE), {
-            padding: 1,
-            margin: 1,
-            borderStyle: 'bold',
-            borderColor: 'yellow',
-          })
-        )
+      // Run report on interval if monikaHQ configuration exists
+      if (!(process.env.CI || process.env.NODE_ENV === 'test')) {
+        loopReport(getConfig)
       }
 
-      if (process.env.NODE_ENV !== 'test') {
-        await notificationChecker(notifications ?? [])
-      }
+      // run probes on interval
+      let abortCurrentLooper: (() => void) | undefined
 
-      console.log(
-        `Starting Monika. Probes: ${probes.length}. Notifications: ${
-          notifications?.length ?? 0
-        }\n`
-      )
-      if (flags.verbose) {
-        console.log('Probes:')
-        probes.forEach((probe) => {
-          console.log(`- Probe ID: ${probe.id}`)
-          console.log(`    Name: ${probe.name}`)
-          console.log(`    Description: ${probe.description}`)
-          console.log(`    Interval: ${probe.interval}`)
-          probe.requests.forEach((request) => {
-            console.log(`    Request Method: ${request.method}`)
-            console.log(`    Request URL: ${request.url}`)
-            console.log(
-              `    Request Headers: ${JSON.stringify(request.headers)}`
-            )
-            console.log(`    Request Body: ${JSON.stringify(request.body)}`)
-          })
-          console.log(`    Alerts: ${probe.alerts.join(', ')}`)
-        })
-        console.log('')
+      for await (const config of getConfigIterator()) {
+        if (abortCurrentLooper) {
+          abortCurrentLooper()
+        }
 
-        console.log(`Notifications:`)
-        notifications?.forEach((item) => {
-          console.log(`- Notification ID: ${item.id}`)
-          console.log(`    Type: ${item.type}`)
-          // Only show recipients if type is mailgun, smtp, or sendgrid
-          if (['mailgun', 'smtp', 'sendgrid'].indexOf(item.type) >= 0) {
-            console.log(
-              `    Recipients: ${(item.data as MailData).recipients.join(', ')}`
-            )
-          }
-          switch (item.type) {
-            case 'smtp':
-              console.log(`    Hostname: ${(item.data as SMTPData).hostname}`)
-              console.log(`    Port: ${(item.data as SMTPData).port}`)
-              console.log(`    Username: ${(item.data as SMTPData).username}`)
-              break
-            case 'mailgun':
-              console.log(`    Domain: ${(item.data as MailgunData).domain}`)
-              break
-            case 'sendgrid':
-              break
-            case 'webhook':
-              console.log(`    URL: ${(item.data as WebhookData).url}`)
-              break
-            case 'slack':
-              console.log(`    URL: ${(item.data as WebhookData).url}`)
-              break
-          }
-        })
-        console.log('')
+        if (process.env.NODE_ENV !== 'test') {
+          await notificationChecker(config.notifications ?? [])
+        }
+
+        const startupMessage = this.buildStartupMessage(config, flags.verbose)
+
+        this.log(startupMessage)
+
+        // Loop through all probes
+        abortCurrentLooper = loopProbes(config)
       }
-      // Loop through all probes
-      looper(config)
     } catch (error) {
       closeLog()
       this.error(error?.message, { exit: 1 })
     }
   }
+
+  buildStartupMessage(config: Config, verbose = false) {
+    const { probes, notifications } = config
+
+    let startupMessage = ''
+
+    // warn if config is empty
+    if ((config.notifications?.length ?? 0) === 0) {
+      const NO_NOTIFICATIONS_MESSAGE = `Notifications has not been set. We will not be able to notify you when an INCIDENT occurs!
+Please refer to the Monika documentations on how to configure notifications at https://hyperjumptech.github.io/monika/guides/notifications.`
+
+      startupMessage += boxen(chalk.yellow(NO_NOTIFICATIONS_MESSAGE), {
+        padding: 1,
+        margin: {
+          top: 2,
+          right: 1,
+          bottom: 2,
+          left: 1,
+        },
+        borderStyle: 'bold',
+        borderColor: 'yellow',
+      })
+    }
+
+    startupMessage += `Starting Monika. Probes: ${
+      probes.length
+    }. Notifications: ${notifications?.length ?? 0}\n\n`
+
+    if (verbose) {
+      startupMessage += 'Probes:\n'
+
+      probes.forEach((probe) => {
+        startupMessage += `- Probe ID: ${probe.id}
+    Name: ${probe.name}
+    Description: ${probe.description}
+    Interval: ${probe.interval}
+`
+        probe.requests.forEach((request) => {
+          startupMessage += `    Request Method: ${request.method}
+    Request URL: ${request.url}
+    Request Headers: ${JSON.stringify(request.headers)}
+    Request Body: ${JSON.stringify(request.body)}
+`
+        })
+
+        startupMessage += `    Alerts: ${probe.alerts.join(', ')}\n`
+      })
+
+      if (notifications && notifications.length > 0) {
+        startupMessage += `\nNotifications:\n`
+
+        notifications.forEach((item) => {
+          startupMessage += `- Notification ID: ${item.id}
+    Type: ${item.type}      
+`
+          // Only show recipients if type is mailgun, smtp, or sendgrid
+          if (['mailgun', 'smtp', 'sendgrid'].indexOf(item.type) >= 0) {
+            startupMessage += `    Recipients: ${(item.data as MailData).recipients.join(
+              ', '
+            )}\n`
+          }
+
+          switch (item.type) {
+            case 'smtp':
+              startupMessage += `    Hostname: ${
+                (item.data as SMTPData).hostname
+              }
+    Port: ${(item.data as SMTPData).port}
+    Username: ${(item.data as SMTPData).username}
+`
+              break
+            case 'mailgun':
+              startupMessage += `    Domain: ${
+                (item.data as MailgunData).domain
+              }\n`
+              break
+            case 'sendgrid':
+              break
+            case 'webhook':
+              startupMessage += `    URL: ${(item.data as WebhookData).url}\n`
+              break
+            case 'slack':
+              startupMessage += `    URL: ${(item.data as WebhookData).url}\n`
+              break
+          }
+        })
+      }
+    }
+
+    return startupMessage
+  }
 }
+
 export = Monika
