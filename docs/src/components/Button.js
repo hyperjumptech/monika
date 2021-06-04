026c5da7e9318b40de7dd95d7afa3d08d495d857
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2021-06-04 10:33:16 +0700

    sample button icons, rename to index-component.js

--- docs/src/components/Button.js
+++ docs/src/components/Button.js
@@ -1,21 +1,55 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2021 Hyperjump Technology                                        *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
 export default function Button(props) {
   if (props.outline) {
-    return (<ButtonOutlined className={props.className}>{props.children}</ButtonOutlined>)
+    return (
+      <ButtonOutlined className={props.className}>
+        {props.children}
+      </ButtonOutlined>
+    )
   }
 
   return (
     <button
-      className={`min-w-md px-4 py-2 bg-gradient-to-r from-aqua to-purple rounded-full font-sans text-white`}
+      className={`min-w-md px-4 py-2 bg-gradient-to-r from-aqua to-purple rounded-full font-sans text-white ${
+        props.className ? props.className : ''
+      }`}
     >
       {props.children}
-    </button>)
+    </button>
+  )
 }
 
 function ButtonOutlined(props) {
   return (
     <button
-      className={`min-w-md px-4 py-2 outline font-sans`}>
-      <span className="bg-gradient-to-r from-aqua to-purple text-transparent bg-clip-text">{props.children}</span>
+      className={`min-w-md px-4 py-2 outline font-sans ${
+        props.className ? props.className : ''
+      }`}
+    >
+      {props.children}
     </button>
   )
 }
--- docs/src/pages/index-component.js
+++ docs/src/pages/index-component.js
@@ -22,13 +22,45 @@
  * SOFTWARE.                                                                      *
  **********************************************************************************/
 
-import Button from "components/Button";
+import Button from 'components/Button'
 
 export default function Home() {
   return (
     <>
-    <p><Button>normal button</Button></p>
-    <p><Button outline>outlined button</Button></p>
+      <p>
+        <Button>normal button</Button>
+      </p>
+      <p>
+        <Button outline>outlined button</Button>
+      </p>
+      <p>
+        <Button outline>
+          <span className="inline-flex items-center fill-current">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 mr-2"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>{' '}
+            star outline button
+          </span>
+        </Button>
+      </p>
+      <p>
+        <Button>
+          <span className="inline-flex items-center fill-current">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 mr-2"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>{' '}
+            star normal button
+          </span>
+        </Button>
+      </p>
     </>
   )
 }
