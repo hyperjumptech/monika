aa1d759226c967fac4d5f5c88e6e4fcdad95f475
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2021-06-08 13:29:09 +0700

    refactor button link

--- docs/src/components/NavBar.js
+++ docs/src/components/NavBar.js
@@ -24,12 +24,12 @@
 
 import Link from 'next/link'
 import { siteConfig } from 'siteConfig'
-import Button from 'components/Button'
+import ButtonLink from 'components/ButtonLink'
 
 export default function NavBar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false)
   return (
-    <nav className="relative flex flex-wrap items-center bg-black-monika justify-between px-2 py-3 mb-3">
+    <nav className="relative flex flex-wrap items-center bg-black-monika justify-between px-2 py-3">
       <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
           <div className="w-60 flex items-center h-16 pt-4 md:pt-0">
@@ -129,28 +129,25 @@ export default function NavBar() {
               </a>
             </li>
             <li className="nav-item">
-              <Button className="ml-4 leading-snug hover:opacity-75">
-                <a
-                  className="flex items-center"
-                  href={siteConfig.repoUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
+              <ButtonLink
+                className="ml-4 flex items-center leading-snug hover:opacity-75"
+                href={siteConfig.repoUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  className="h-6 w-6"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
                 >
-                  <span className="sr-only">GitHub</span>
-                  <svg
-                    className="h-6 w-6"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                      clipRule="evenodd"
-                    />
-                  </svg>{' '}
-                  Github
-                </a>
-              </Button>
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                    clipRule="evenodd"
+                  />
+                </svg>{' '}
+                Github
+              </ButtonLink>
             </li>
           </ul>
         </div>
