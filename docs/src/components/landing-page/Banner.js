e28314b6c66f4d055bebe32a2a9d7e5e271ad1af
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2021-06-09 12:32:52 +0700

    spacing between

--- docs/src/components/landing-page/Banner.js
+++ docs/src/components/landing-page/Banner.js
@@ -1,3 +1,27 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2021 Hyperjump Technology                                        *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
 import ButtonLink from 'components/ButtonLink'
 
 export default function Banner() {
@@ -13,36 +37,42 @@ export default function Banner() {
         </p>
         <p>Let's get started in seconds.</p>
       </div>
-      <div className="m-auto mt-4 px-16 py-4 bg-gray-monika bg-opacity-10 rounded-md">
-        <p className="bg-gradient-to-r from-purple-monika to-aqua-monika bg-clip-text font-normal text-center text-transparent">
-          npm i -g @hyperjumptech/monika
+      <div className="m-auto">
+        <p className="mt-4 px-16 py-4 bg-gray-monika bg-opacity-10 rounded-md">
+          <span className="bg-gradient-to-r from-purple-monika to-aqua-monika bg-clip-text font-normal text-center text-transparent">
+            npm i -g @hyperjumptech/monika
+          </span>
         </p>
-      </div>
-      <div className="flex flex-row justify-between m-auto mt-4">
-        <ButtonLink
-          className="w-40 flex items-center leading-snug hover:opacity-75 font-semibold text-center"
-          href="/quick-start"
-          rel="noopener noreferrer"
-        >
-          Get Started!
-        </ButtonLink>
-        <ButtonLink
-          outline="true"
-          className="w-40 flex items-center leading-snug hover:opacity-75 font-semibold"
-          href="/quick-start"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 mr-2"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-          </svg>{' '}
-          Star
-        </ButtonLink>
+        <div className="flex flex-row justify-between mt-4">
+          <div className="flex">
+            <ButtonLink
+              className="w-40 leading-snug font-semibold text-center"
+              href="/quick-start"
+              rel="noopener noreferrer"
+            >
+              Get Started!
+            </ButtonLink>
+          </div>
+          <div className="flex">
+            <ButtonLink
+              outline="true"
+              className="w-40 flex justify-center leading-snug font-semibold text-center"
+              href="/quick-start"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>{' '}
+              Star
+            </ButtonLink>
+          </div>
+        </div>
       </div>
     </div>
   )
