20dacfae5deff5eaad5bb430bb9dbbf6a227bb88
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2021-06-11 17:29:57 +0700

    refactor

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,3 +1,27 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2021 Hyperjump Technology                                        *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
 // tailwind.config.js
 
 const defaultTheme = require('tailwindcss/defaultTheme')
@@ -48,6 +72,10 @@ module.exports = {
       height: {
         '3/4': '75vh',
       },
+      inset: {
+        '-16': '-4rem',
+        '-20': '-6rem',
+      },
       margin: {
         '-y-3/4': '-75vh',
         '-y-1/4': '-25vh',
@@ -55,10 +83,6 @@ module.exports = {
       },
       opacity: {
         10: '0.1',
-      },
-      inset: {
-        '-16': '-4rem',
-        '-20': '-6rem',
       },
     },
     fontFamily: {
