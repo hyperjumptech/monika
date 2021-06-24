f83a209b0edd33a96dbb36558ac357eeea5bc08f
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2021-06-24 17:45:31 +0700

    remove unnecessary prop check, change max height

--- docs/src/components/StarButton.js
+++ docs/src/components/StarButton.js
@@ -1,3 +1,27 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2021 Hyperjump Technology                                        *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
 import ButtonLink from 'components/ButtonLink'
 import { useEffect, useState } from 'react'
 import { siteConfig } from 'siteConfig'
@@ -19,9 +43,7 @@ export default function StarButton(props) {
   return (
     <ButtonLink
       outline="true"
-      className={`w-40 flex justify-center leading-snug font-semibold text-center max-h-10 ${
-        props.className ? props.className : ''
-      }`}
+      className={`w-40 flex justify-center leading-snug font-semibold text-center max-h-11`}
       href={siteConfig.repoUrl}
       target="_blank"
       rel="noopener noreferrer"
