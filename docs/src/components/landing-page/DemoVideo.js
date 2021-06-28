9e466813796e4d52d3a93eb32429d251b1ff91ca
Mochamad Noor Syamsu <samsunoor@users.noreply.github.com>
2021-06-28 10:28:45 +0700

    Responsive mobile home page (#252)
    * responsive mobile page
    
    * fix text margin mobile view
    
    * clean up text size on mobile view
    
    * clean up padding
    
    * clean up margin on medium and large width
    
    Co-authored-by: Denny Pradipta <denny@hyperjump.tech>

--- docs/src/components/landing-page/DemoVideo.js
+++ docs/src/components/landing-page/DemoVideo.js
@@ -27,11 +27,11 @@ import WorldDotPng from '../../../public/world-dot.png'
 
 export default function DemoVideo() {
   return (
-    <div className="relative flex flex-col justify-center pt-1/3 xl:pt-1/5">
+    <div className="relative flex flex-col justify-center pt-64 md:pt-2/5 lg:pt-1/4 xl:pt-1/5">
       <img
         src={WorldDotPng}
         style={{ marginTop: '-50rem' }}
-        className="absolute top-0 right-0"
+        className="absolute top-0 right-0 xl:block hidden"
       />
       <div className="m-auto flex flex-col z-10">
         <h3 className="m-auto text-white text-xl -mt-y-1/4">
@@ -42,27 +42,27 @@ export default function DemoVideo() {
         </h3>
         <div className="flex flex-wrap justify-around gap-8 mt-8">
           <iframe
-            className="my-auto w-96 h-72"
+            className="my-auto md:w-96 md:h-72 w-56 h-32"
             src="https://www.youtube-nocookie.com/embed/o4jrNeNeFmM"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen={true}
           />
-          <div className="m-auto flex flex-col max-w-sm">
-            <p className="text-white text-4xl leading-none">
+          <div className="mx-8 md:m-auto flex flex-col max-w-sm">
+            <p className="text-white text-3xl lg:text-4xl leading-none lg:text-left text-center">
               Sleep well knowing that your{' '}
               <span className="bg-gradient-to-r from-purple-monika to-aqua-monika bg-clip-text text-transparent font-bold">
                 server is working as it should be
               </span>
             </p>
-            <p className="text-white mt-4 leading-tight">
+            <p className="text-white mt-4 leading-tight lg:text-left text-center">
               Monitor every part of your web app using a simple JSON
               configuration file. Get alerts not only when your site is down,
               but also when it's slow.
             </p>
             <ButtonLink
-              className="mt-6 w-40 leading-snug font-semibold text-sm text-center"
+              className="mt-6 w-40 lg:ml-0 mx-auto leading-snug font-semibold text-sm text-center"
               href="/quick-start"
               rel="noopener noreferrer"
             >
