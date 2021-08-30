145c60c6b6ecf06a213eb13efb9b0904259a77be
Armedi <armedi@outlook.com>
2021-08-30 17:54:49 +0700

    feat: send status-update notification via monika-notif channel

--- src/components/notification/index.ts
+++ src/components/notification/index.ts
@@ -174,11 +174,30 @@ export async function sendNotifications(
                 alert: message.summary,
                 url: message.meta.url,
                 time: message.meta.time,
-                monika: `${message.meta.privateIpAddress} (local), ${
-                  message.meta.publicIpAddress
-                    ? `${message.meta.publicIpAddress} (public)`
-                    : ''
-                } ${message.meta.hostname} (hostname)`,
+                monika: `${message.meta.hostname} (${[
+                  message.meta.publicIpAddress,
+                  message.meta.privateIpAddress,
+                ]
+                  .filter(Boolean)
+                  .join('/')})`,
+              }
+            } else if (message.meta.type === 'status-update') {
+              body = {
+                type: message.meta.type,
+                time: message.meta.time,
+                monika: `${message.meta.hostname} (${[
+                  message.meta.publicIpAddress,
+                  message.meta.privateIpAddress,
+                ]
+                  .filter(Boolean)
+                  .join('/')})`,
+                numberOfProbes: String(message.meta.numberOfProbes),
+                averageResponseTime: String(message.meta.averageResponseTime),
+                numberOfIncidents: String(message.meta.numberOfIncidents),
+                numberOfRecoveries: String(message.meta.numberOfRecoveries),
+                numberOfSentNotifications: String(
+                  message.meta.numberOfSentNotifications
+                ),
               }
             }
 
