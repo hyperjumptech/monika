1ba8117348de072bef6d8fe2fffbbb044d6047bd
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2021-09-02 19:34:35 +0700

    remove duplicate

--- test/components/notification/checker.smtp.test.ts
+++ test/components/notification/checker.smtp.test.ts
@@ -1,3 +1,27 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2021 Hyperjump Technology                                        *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
 import chai, { expect } from 'chai'
 import spies from 'chai-spies'
 
@@ -39,28 +63,6 @@ describe('notificationChecker - smtpNotification', () => {
       ])
 
     expect(fn).not.to.throws()
-  })
-
-  it('should handle validation error - without hostname', async () => {
-    try {
-      await notificationChecker([
-        {
-          ...smtpNotificationConfig,
-          data: {
-            port: 1000,
-            username: 'username',
-            password: 'password',
-          } as SMTPData,
-        },
-      ])
-    } catch (error) {
-      const originalErrorMessage = '"SMTP Hostname" is required'
-      const { message } = errorMessage('SMTP', originalErrorMessage)
-
-      expect(() => {
-        throw error
-      }).to.throw(message)
-    }
   })
 
   it('should handle validation error - without hostname', async () => {
