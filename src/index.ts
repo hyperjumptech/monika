151db771e83dc516884a82e390f9e3b238abcca9
Armedi <armedi@outlook.com>
2021-09-17 04:47:11 +0700

    Changed: Attach incident and recovery thresholds calculation to each request and per alert (#410)
    * feat: add alerts to request config
    
    * refactor: rename ValidateResponse => ValidatedResponse
    
    * refactor: rename hasSomethingToReport => isAlertTriggered
    
    * refactor: rename validatedRes => validatedResponse
    
    * refactor: remove unused processThresholds function parameters
    
    * feat: move threshold check to after each request
    
    * refactor: remove unnecessary data in ProbeStatus
    
    * Revert "feat: add alerts to request config"
    
    This reverts commit 52ca8c62fba834fec4253ab975ace9dc6529bd3b.
    
    * refactor: rename ProbeStatus => ServerState
    
    * refactor: use Map to store serverStates
    
    * refactor: use xstate to process server states transitions
    
    * test: fix failed test caused by property name change
    
    * refactor: change property names in state machine context
    
    * refactor: remove unnecessary try-catch block
    
    * test: create test for serverAlertStateMachine
    
    * refactor: change array methods to more suitable ones in component/probe/index.ts
    
    * refactor: remove unnecessary variable reassignment because it's already mutated
    
    * test: create test for processThresholds
    
    Co-authored-by: Nico Prananta <nicnocquee@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ import {
 } from './components/logger/history'
 import { sendAlerts, sendNotifications } from './components/notification'
 import { notificationChecker } from './components/notification/checker'
-import { resetProbeStatuses } from './components/notification/process-server-status'
+import { resetServerAlertStates } from './components/notification/process-server-status'
 import { checkTLS } from './components/tls-checker'
 import events from './events'
 import { Config } from './interfaces/config'
@@ -217,7 +217,7 @@ class Monika extends Command {
 
       for await (const config of getConfigIterator()) {
         if (abortCurrentLooper) {
-          resetProbeStatuses()
+          resetServerAlertStates()
           abortCurrentLooper()
         }
 
@@ -456,7 +456,7 @@ Please refer to the Monika documentations on how to how to configure notificatio
             notifications: notifications ?? [],
             validation: {
               alert: { query: '', message: error.message },
-              hasSomethingToReport: true,
+              isAlertTriggered: true,
               response: {
                 status: 500,
                 config: {
