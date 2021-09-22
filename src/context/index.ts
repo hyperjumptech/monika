f62027aeb11354551a5f29a86288885679c4174f
Hari Cahya Nugraha <haricnugraha@gmail.com>
2021-09-22 18:30:38 +0700

    fix: undefined app version when not running use npm

--- src/context/index.ts
+++ src/context/index.ts
@@ -22,58 +22,19 @@
  * SOFTWARE.                                                                      *
  **********************************************************************************/
 
-import type { IConfig } from '@oclif/config'
-import { getConfig, setupConfig } from '../components/config'
-import { setContext } from '../context'
-import events from '../events'
-import { tlsChecker } from '../jobs/tls-check'
-import { loopCheckSTUNServer, loopReport } from '../looper'
-import {
-  PrometheusCollector,
-  startPrometheusMetricsServer,
-} from '../plugins/metrics/prometheus'
-import { getEventEmitter } from '../utils/events'
-import { getPublicNetworkInfo } from '../utils/public-ip'
-// import to activate all the application event emitter subscribers
-import '../events/subscribers/application'
-import { jobsLoader } from './jobs'
+type Context = {
+  // userAgent example: @hyperjumptech/monika/1.2.3 linux-x64 node-14.17.0
+  userAgent: string
+}
 
-export default async function init(flags: any, cliConfig: IConfig) {
-  const eventEmitter = getEventEmitter()
-  const isTestEnvironment = process.env.CI || process.env.NODE_ENV === 'test'
+let context = {
+  userAgent: '',
+}
 
-  setContext({ userAgent: cliConfig.userAgent })
+export function getContext(): Context {
+  return context
+}
 
-  // cache location & ISP info
-  await getPublicNetworkInfo()
-  // check if connected to STUN Server and getting the public IP in the same time
-  loopCheckSTUNServer(flags.stun)
-
-  // start Promotheus server
-  if (flags.prometheus) {
-    const {
-      registerCollectorFromProbes,
-      collectProbeRequestMetrics,
-    } = new PrometheusCollector()
-
-    // register prometheus metric collectors
-    eventEmitter.on(events.config.sanitized, registerCollectorFromProbes)
-    // collect prometheus metrics
-    eventEmitter.on(events.probe.response.received, collectProbeRequestMetrics)
-
-    startPrometheusMetricsServer(flags.prometheus)
-  }
-
-  await setupConfig(flags)
-
-  // check TLS when Monika starts
-  tlsChecker()
-
-  if (!isTestEnvironment) {
-    // load cron jobs
-    jobsLoader()
-
-    // Run report on interval if symon configuration exists
-    loopReport(getConfig)
-  }
+export function setContext(updatedContext: Context) {
+  context = { ...context, ...updatedContext }
 }
