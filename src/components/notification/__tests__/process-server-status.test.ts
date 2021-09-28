900143f5341c52dcd0df0de3ce5523e158b14ef5
Armedi <armedi@outlook.com>
2021-09-28 12:55:03 +0700

    Fix: Notification was sent when no state transition happen (#445)
    * fix: previous state is not taken into consideration when calculating shouldSendNotification value
    
    * test: add test for shouldSendNotification flag computation
    
    * refactor: remove unnecessary extra condition for shouldSendNotification computation
    
    * test: update test case for processThresholds shouldSendNotification computation

--- src/components/notification/__tests__/process-server-status.test.ts
+++ src/components/notification/__tests__/process-server-status.test.ts
@@ -25,6 +25,7 @@
 import { expect } from 'chai'
 import { interpret, Interpreter } from 'xstate'
 import { Probe } from '../../../interfaces/probe'
+import { ServerAlertState } from '../../../interfaces/probe-status'
 import { ValidatedResponse } from '../../../plugins/validate-response'
 import {
   processThresholds,
@@ -43,7 +44,6 @@ describe('serverAlertStateMachine', () => {
         recoveryThreshold: 5,
         consecutiveFailures: 0,
         consecutiveSuccesses: 0,
-        hasBeenDownAtLeastOnce: false,
       })
     ).start()
   })
@@ -125,45 +125,52 @@ describe('serverAlertStateMachine', () => {
 })
 
 describe('processThresholds', () => {
+  const probe = {
+    requests: [
+      {
+        method: 'GET',
+        url: 'https://httpbin.org/status/200',
+      },
+      {
+        method: 'POST',
+        url: 'https://httpbin.org/status/201',
+      },
+    ],
+    incidentThreshold: 2,
+    recoveryThreshold: 2,
+  } as Probe
+
+  const successResponse = [
+    {
+      alert: { query: 'response.time > 1000' },
+      isAlertTriggered: false,
+    },
+  ] as ValidatedResponse[]
+
+  const failureResponse = [
+    {
+      alert: { query: 'response.time > 1000' },
+      isAlertTriggered: true,
+    },
+  ] as ValidatedResponse[]
+
   beforeEach(() => {
     resetServerAlertStates()
   })
 
-  it('should attach threshold calculation to each request', () => {
-    const probe = {
-      requests: [
-        {
-          method: 'GET',
-          url: 'https://httpbin.org/status/200',
-        },
-        {
-          method: 'POST',
-          url: 'https://httpbin.org/status/201',
-        },
-      ],
-      incidentThreshold: 2,
-      recoveryThreshold: 2,
-    } as Probe
-
-    const validatedResponse = [
-      {
-        alert: { query: 'response.time > 1000' },
-        isAlertTriggered: true,
-      },
-    ] as ValidatedResponse[]
-
+  it('should attach state calculation to each request', () => {
     // failure happened first for request with index 0
     processThresholds({
       probe,
       requestIndex: 0,
-      validatedResponse,
+      validatedResponse: failureResponse,
     })
 
     // processing request with index 1
     const result1 = processThresholds({
       probe,
       requestIndex: 1,
-      validatedResponse,
+      validatedResponse: failureResponse,
     })
 
     // failure happened only once for request with index 1, it does not reach threshold yet
@@ -172,10 +179,84 @@ describe('processThresholds', () => {
     const result2 = processThresholds({
       probe,
       requestIndex: 1,
-      validatedResponse,
+      validatedResponse: failureResponse,
     })
 
     // second time failure happened for request with index 1, it reaches threshold
     expect(result2[0].state).to.equals('DOWN')
+  })
+
+  it('should compute shouldSendNotification based on threshold and previous state', () => {
+    let result!: ServerAlertState[]
+
+    // trigger down state
+    for (let i = 0; i < probe.incidentThreshold; i++) {
+      result = processThresholds({
+        probe,
+        requestIndex: 1,
+        validatedResponse: failureResponse,
+      })
+    }
+
+    expect(result[0].state).to.equals('DOWN')
+
+    // initial state is UP and now it is changed to DOWN, should send notification
+    expect(result[0].shouldSendNotification).to.equals(true)
+
+    // send success response once
+    result = processThresholds({
+      probe,
+      requestIndex: 1,
+      validatedResponse: successResponse,
+    })
+
+    // send failure response again as much threshold
+    for (let i = 0; i < probe.incidentThreshold; i++) {
+      result = processThresholds({
+        probe,
+        requestIndex: 1,
+        validatedResponse: failureResponse,
+      })
+    }
+
+    expect(result[0].state).to.equals('DOWN')
+
+    // should not send notification again since state does not change
+    expect(result[0].shouldSendNotification).to.equals(false)
+
+    // send success response as much threshold
+    for (let i = 0; i < probe.recoveryThreshold; i++) {
+      result = processThresholds({
+        probe,
+        requestIndex: 1,
+        validatedResponse: successResponse,
+      })
+    }
+
+    expect(result[0].state).to.equals('UP')
+
+    // should send notification since state change from DOWN to UP
+    expect(result[0].shouldSendNotification).to.equals(true)
+
+    // send failure response once
+    result = processThresholds({
+      probe,
+      requestIndex: 1,
+      validatedResponse: failureResponse,
+    })
+
+    // send success response as much threshold
+    for (let i = 0; i < probe.recoveryThreshold; i++) {
+      result = processThresholds({
+        probe,
+        requestIndex: 1,
+        validatedResponse: successResponse,
+      })
+    }
+
+    expect(result[0].state).to.equals('UP')
+
+    // should not send notification again since state does not change
+    expect(result[0].shouldSendNotification).to.equals(false)
   })
 })
