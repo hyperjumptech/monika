c691cff3acac47d10f14993b02b2e3a8a29ce94d
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2021-09-30 10:49:39 +0800

    move version to new line

--- src/jobs/summary-notification.ts
+++ src/jobs/summary-notification.ts
@@ -61,6 +61,7 @@ Version: ${userAgent}`,
         hostname: hostname(),
         privateIpAddress: getIp(),
         publicIpAddress,
+        version: userAgent,
         ...summary,
       },
     }).catch((error) => log.error(`Summary notification: ${error.message}`))
