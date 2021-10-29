9e3d0e52ceb0b09d9097c64b690a215cd857ef3a
muslim.ilmiawan <muslim.ilmiawan@gmail.com>
2021-10-29 16:25:36 +0700

    fix(notification): don't send status notification in symon mode

--- src/index.ts
+++ src/index.ts
@@ -278,7 +278,8 @@ class Monika extends Command {
         // schedule status update notification
         if (
           process.env.NODE_ENV !== 'test' &&
-          flags['status-notification'] !== 'false'
+          flags['status-notification'] !== 'false' &&
+          !config.symon
         ) {
           // defaults to 6 AM
           // default value is not defined in flag configuration,
