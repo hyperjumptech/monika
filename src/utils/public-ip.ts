fdef8ca0b08dc8d140df9411a67f7e89e658cb57
budhi <sapiderman.lumpat@gmail.com>
2022-01-17 09:31:51 +0700

    remove test bypass for test, fix timout typo

--- src/utils/public-ip.ts
+++ src/utils/public-ip.ts
@@ -33,15 +33,14 @@ export let publicIpAddress = ''
 export let isConnectedToSTUNServer = true
 export let publicNetworkInfo: { country: string; city: string; isp: string }
 
-const isTestEnvironment = process.env.NODE_ENV === 'test' || process.env.CI
-
 /**
  * pokeStun sends a poke/request to stun server
  * @returns {Promise<string>}
  */
 async function pokeStun(): Promise<string> {
+  
   const connection = await sendPing('stun.l.google.com')
-  if (connection.alive || isTestEnvironment) {
+  if (connection.alive) {
     const response = await stun.request('stun.l.google.com:19302')
     return response?.getXorAddress()?.address
   }
