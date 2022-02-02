78994057bff6bd95019c5316e402de1b922f2e52
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2022-02-02 13:48:38 +0800

    fails update rc files

--- scripts/monika-install.sh
+++ scripts/monika-install.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-echo ''
-echo '███╗   ███╗ ██████╗ ███╗   ██╗██╗██╗  ██╗ █████╗ '
-echo '████╗ ████║██╔═══██╗████╗  ██║██║██║ ██╔╝██╔══██╗'
-echo '██╔████╔██║██║   ██║██╔██╗ ██║██║█████╔╝ ███████║'
-echo '██║╚██╔╝██║██║   ██║██║╚██╗██║██║██╔═██╗ ██╔══██║'
-echo '██║ ╚═╝ ██║╚██████╔╝██║ ╚████║██║██║  ██╗██║  ██║'
-echo '╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝'
-echo ''
+echo ""
+echo "███╗   ███╗ ██████╗ ███╗   ██╗██╗██╗  ██╗ █████╗ "
+echo "████╗ ████║██╔═══██╗████╗  ██║██║██║ ██╔╝██╔══██╗"
+echo "██╔████╔██║██║   ██║██╔██╗ ██║██║█████╔╝ ███████║"
+echo "██║╚██╔╝██║██║   ██║██║╚██╗██║██║██╔═██╗ ██╔══██║"
+echo "██║ ╚═╝ ██║╚██████╔╝██║ ╚████║██║██║  ██╗██║  ██║"
+echo "╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝"
+echo ""
 
 # main repo url
 repo_url="https://github.com/hyperjumptech/monika/"
@@ -19,23 +19,23 @@ version_to_install="latest"
 install_dir=$HOME"/.monika"
 
 binary_url() {
-  local version_name="$1"
-  local os="$2"
+  version_name="$1"
+  os="$2"
   echo $repo_url"releases/download/v$version_name/monika-v$version_name-$os-x64.zip"
 }
 
 info() {
-  local action="$1"
-  local details="$2"
-  command printf '\033[1;32m%12s\033[0m %s\n' "$action" "$details" 1>&2
+  action="$1"
+  details="$2"
+  command printf "\033[1;32m%12s\033[0m %s\n" "$action" "$details" 1>&2
 }
 
 error() {
-  command printf '\033[1;31mError\033[0m: %s\n\n' "$1" 1>&2
+  command printf "\033[1;31mError\033[0m: %s\n\n" "$1" 1>&2
 }
 
 warning() {
-  command printf '\033[1;33mWarning\033[0m: %s\n\n' "$1" 1>&2
+  command printf "\033[1;33mWarning\033[0m: %s\n\n" "$1" 1>&2
 }
 
 # sanity checks
@@ -65,16 +65,16 @@ END_HELP
 }
 
 sanitize_version() {
-  echo "$1" | grep -oP '([0-9]+(\.[0-9]+)+)'
+  echo "$1" | grep -oP "([0-9]+(\.[0-9]+)+)"
 }
 
 get_latest_version() {
-  local url=$repo_url"releases/latest"
-  echo $(sanitize_version "$(curl -iLs -o /dev/null -w %{url_effective} $url)")
+  url=$repo_url"releases/latest"
+  echo "$(sanitize_version $(curl -iLs -o /dev/null -w %{url_effective} $url))"
 }
 
 get_os_name() {
-  local os=$(uname -s)
+  os=$(uname -s)
   case "$os" in
     Darwin)
       echo "macos"
@@ -92,62 +92,62 @@ get_os_name() {
 }
 
 install_from_file() {
-  unzip -o $1 -d $install_dir
-  chmod +x $install_dir"/monika"
-  rm $1
+  unzip -o "$1" -d "$install_dir"
+  chmod +x "$install_dir/monika"
+  rm "$1"
 }
 
 install_release_version() {
-  local os=$(get_os_name)
-  local version_name="$1"
-  local url=$(binary_url "$version_name" "$os")
-  local target_path=$install_dir"/monika-v$version_name-$os-x64.zip"
-  if [ ! -d $install_dir ]; then
-    mkdir $install_dir
+  os=$(get_os_name)
+  version_name="$1"
+  url=$(binary_url "$version_name" "$os")
+  target_path="$install_dir/monika-v$version_name-$os-x64.zip"
+  if [ ! -d "$install_dir" ]; then
+    mkdir "$install_dir"
   fi
   
-  if [ ! -f $target_path ]; then
-    info "Downloading from: "$url
+  if [ ! -f "$target_path" ]; then
+    info "Downloading from: $url"
     # download and overwrite target path
-    curl -L $url > $target_path
+    curl -L "$url" > "$target_path"
   fi
-  install_from_file $target_path
+  install_from_file "$target_path"
 }
 
 monika_home_is_ok() {
-  if [ -f $install_dir ]; then
-    error $install_dir" is a file. Please remove it first before installation."
+  if [ -f "$install_dir" ]; then
+    error "$install_dir is a file. Please remove it first before installation."
     return 1
   fi
   return 0
 }
 
 install_version() {
-  local version_to_install="$1"
+  version_to_install="$1"
   if ! monika_home_is_ok; then
     exit 1
   fi
   case "$version_to_install" in
     latest)
-      local latest_version="$(get_latest_version)"
+      latest_version="$(get_latest_version)"
       info "Installing latest version of Monika ($latest_version)"
-      install_release_version $latest_version $install_dir
+      install_release_version "$latest_version" "$install_dir"
       ;;
     *) # assume anything else is monika version
-      local version_name=$(sanitize_version "$version_to_install")
+      version_name=$(sanitize_version "$version_to_install")
       info "Installing Monika version $version_name"
-      install_release_version $version_name $install_dir
+      install_release_version "$version_name" "$install_dir"
       ;;
   esac
 }
 
 check_architecture() {
-  local arch="$(uname -m)"
-  if [ $arch = "x86_64" ]; then
+  arch="$(uname -m)"
+  if [ "$arch" = "x86_64" ]; then
     return 0
   fi
 
-  if [ $arch = "arm64" ] && [ "$(uname -s)" = "Darwin" ]; then
+  if [ "$arch" = "arm64" ] && [ "$(uname -s)" = "Darwin" ]; then
     return 0
   fi
 
@@ -175,17 +175,16 @@ check_architecture
 
 install_version "$version_to_install" "$install_dir"
 
-# if [ -f $HOME"/.bashrc" ] && [ -z $(grep '.monika' $HOME"/.bashrc") ]; then
-if [ -f $HOME"/.bashrc" ]; then
-  info 'Updating .bashrc file...'
-  cat "export PATH=\$PATH:$install_dir" >> $HOME"/.bashrc"
+if [ -f "$HOME/.bashrc" ] && [ -z "$(grep \".monika\" \"$HOME/.bashrc\")" ]; then
+  info "Updating .bashrc file..."
+  cat "export PATH=\$PATH:$install_dir" >> "$HOME/.bashrc"
+  export PATH="$PATH:$install_dir"
 fi
 
-if [ -f $HOME"/.zshrc" ] && [ -z $(grep '.monika' $HOME"/.zshrc") ]; then
-  info 'Updating .zshrc file...'
-  cat "export PATH=\$PATH:$install_dir" >> $HOME"/.zshrc"
+if [ -f "$HOME/.zshrc" ] && [ -z "$(grep \".monika\" \"$HOME/.bashrc\")" ]; then
+  info "Updating .zshrc file..."
+  cat "export PATH=\$PATH:$install_dir" >> "$HOME/.zshrc"
+  export PATH="$PATH:$install_dir"
 fi
-
-export PATH=$PATH:$install_dir
 
 info "Installation done. You can now start using monika."
