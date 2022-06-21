10479ff5ccfd31cfeda52a21d1a8874b6ea6b0d6
Lukman Adisaputro <lukman.adisaputro@gmail.com>
2022-06-21 14:37:57 +0700

    remove keys

--- docs/src/siteConfig.js
+++ docs/src/siteConfig.js
@@ -5,10 +5,8 @@ export const siteConfig = {
   npmUrl: 'https://www.npmjs.com/package/@hyperjumptech/monika',
   repoUrl: 'https://github.com/hyperjumptech/monika',
   algolia: {
-    appId: process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID || '5O2YZGK4NG',
-    apiKey:
-      process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY ||
-      '65ea934da001128cb8293359a5e67a12',
+    appId: process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID || '',
+    apiKey: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || '',
     indexName: 'monika-documentation',
     // algoliaOptions: {
     //   facetFilters: ['version:VERSION'],
