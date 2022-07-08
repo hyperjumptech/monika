823db88a6d98e20ca8eb3eaf16aa8f3f0b190a11
Lukman Adisaputro <42835003+lukman7788@users.noreply.github.com>
2022-07-08 14:26:17 +0700

    support reading sitemap (#751)
    * sitemap support
    
    * sitemap support
    
    * update xml
    
    * Update parse-sitemap.ts
    
    * remove short flag
    
    * fixing multi langguage support
    
    * update lisence
    
    * update lisence
    
    * update lisence
    
    * update wording
    
    * fixing package.json
    
    * fixing package.json
    
    * fixing package.json
    
    * update documentation
    
    * remove cred
    
    * remove unused code
    
    * update typo
    
    * update wording
    
    Co-authored-by: Nico Prananta <nicnocquee@users.noreply.github.com>

---
+++ src/components/config/parse-sitemap.ts
@@ -0,0 +1,73 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2021 Hyperjump Technology                                        *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
+import { Config } from '../../interfaces/config'
+import { DEFAULT_THRESHOLD } from '../../looper'
+import { XMLParser } from 'fast-xml-parser'
+
+const generateProbesFromXml = (config: any) => {
+  const probes = config?.urlset?.url?.map((item: any) => {
+    // eslint-disable-next-line prefer-const
+    let requests = [
+      {
+        url: item?.loc,
+        method: 'GET',
+        timeout: 10_000,
+      },
+    ]
+    if (item['xhtml:link']) {
+      for (const alt of item['xhtml:link']) {
+        requests.push({
+          url: alt['@_href'],
+          method: 'GET',
+          timeout: 10_000,
+        })
+      }
+    }
+    return {
+      id: item?.loc,
+      name: item?.loc,
+      requests: requests,
+      incidentThreshold: DEFAULT_THRESHOLD,
+      recoveryThreshold: DEFAULT_THRESHOLD,
+      alerts: [],
+    }
+  })
+
+  return probes ?? []
+}
+
+export const parseConfigFromSitemap = (configString: string): Config => {
+  try {
+    const parser = new XMLParser({ ignoreAttributes: false })
+    const xmlObj = parser.parse(configString)
+    const probes = generateProbesFromXml(xmlObj)
+
+    return { probes }
+  } catch (error: any) {
+    throw new Error(
+      'Your Sitemap file contains an invalid Sitemap XML format !'
+    )
+  }
+}
