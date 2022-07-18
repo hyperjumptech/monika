a50b64c03a4bffdfd49e8bc0d5a51554ca8dbb72
Budhi <964106+sapiderman@users.noreply.github.com>
2022-07-18 17:32:55 +0700

    Add test to validate example configs (#773)
    * add monika-configuration.json
    
    * add checking for notifications
    
    * add explicit requirement for data:url
    
    * add more offline test cases
    
    * ran prettier
    
    * update schema filename
    
    * add body and headers
    
    * ran formatter
    
    * cleanup test file
    
    * remove array body and headers
    
    * update filename
    
    * free form body
    
    * add url validation
    
    * add test, fix body validation
    
    * oops..removed *.only test debug
    
    * update variable name from PR review
    
    * use of const instead of enum
    
    * test sample configs
    
    * use latest schema, remove test.only
    
    Co-authored-by: Gregory Sukanto <gregory.sukanto@gmail.com>
    Co-authored-by: Nico Prananta <nicnocquee@users.noreply.github.com>

---
+++ test/json-schema/validate-sample-configs.test.ts
@@ -0,0 +1,85 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2021 Hyperjump Technology                                        *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
+import yaml from 'js-yaml'
+import fs from 'fs'
+import path from 'path'
+import { expect } from 'chai'
+import Ajv from 'ajv'
+
+const ajv = new Ajv()
+
+import mySchema from '../../monika-config-schema.json'
+
+ajv.addVocabulary(['name', 'fileMatch', 'url']) // add custom Scheme Store keywords, reference: https://www.schemastore.org/json/
+
+const validate = ajv.compile(mySchema)
+
+const getAllFiles = function (dirPath: string, arrayOfFiles: string[]) {
+  const files = fs.readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  // files.forEach(function(file) {
+  for (const file of files) {
+    if (fs.statSync(dirPath + '/' + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(path.join(dirPath, '/', file))
+    }
+  }
+
+  return arrayOfFiles
+}
+
+describe('validate example configs', () => {
+  it('should detect examples that does not conform to the schema', () => {
+    const files = getAllFiles('./config_sample', [])
+
+    let sampleFile: any
+
+    for (const file of files) {
+      switch (path.extname(file)) {
+        case '.yml':
+        case '.yaml':
+          sampleFile = yaml.load(fs.readFileSync(file, 'utf8'))
+          break
+        case '.json':
+          sampleFile = JSON.parse(fs.readFileSync(file, 'utf8'))
+          break
+        default:
+          sampleFile = null
+          continue // skip for other file.extension
+      }
+
+      const isValid = validate(sampleFile)
+      if (isValid === false) {
+        console.error('file:', file, 'validity:', isValid)
+        console.error('error:', validate.errors)
+      }
+
+      expect(isValid).to.be.true
+    }
+  })
+})
