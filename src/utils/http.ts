e8a936ddeaf75137bbf63dceffbfe06d67db12f6
budhi <sapiderman.lumpat@gmail.com>
2022-10-17 17:58:37 +0700

    pr feedback

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,3 +1,27 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2021 Hyperjump Technology                                        *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 import http from 'http'
 import https from 'https'
@@ -16,9 +40,9 @@ export async function sendHttpRequest(
 ): Promise<AxiosResponse> {
   const resp = await axiosInstance.request({
     ...config,
-    timeout: config.timeout ?? DEFAULT_TIMEOUT,
-    httpAgent,
-    httpsAgent,
+    timeout: config.timeout ?? DEFAULT_TIMEOUT, // Ensure default timeout if not filled.
+    httpAgent: config.httpAgent ?? httpAgent,
+    httpsAgent: config.httpsAgent ?? httpsAgent,
   })
 
   return resp
