b480d55347fc4712f84fff053447dc8f876841a2
budhi <sapiderman.lumpat@gmail.com>
2023-02-20 14:29:19 +0700

    sync to main and pr

--- src/components/config/validate.ts
+++ src/components/config/validate.ts
@@ -50,7 +50,7 @@ export const validateConfig = (configuration: Config): Validation => {
 
   // check config file against monika-config-schema.json only if a configfile is passed
   if (flags.config.length > 0) {
-    const isValidConfig = validator.validateConfigFile(flags.config[0])
+    const isValidConfig = validator.validateConfigWithSchema(configuration)
     if (isValidConfig.valid === false) return isValidConfig
   }
 
