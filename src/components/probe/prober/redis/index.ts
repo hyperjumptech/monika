e314f3bbec2a0aa407406cca30f785da61b2aff9
budhi <sapiderman.lumpat@gmail.com>
2023-05-04 07:21:56 +0700

    adding redis url support

--- src/components/probe/prober/redis/index.ts
+++ src/components/probe/prober/redis/index.ts
@@ -1,3 +1,4 @@
+import parse from 'url-parse'
 import type { ProbeResult } from '..'
 import type { Redis } from '../../../../interfaces/probe'
 import { redisRequest } from './request'
@@ -14,16 +15,32 @@ export async function probeRedis({
   redis,
 }: ProbeRedisParams): Promise<ProbeResult[]> {
   const probeResults: ProbeResult[] = []
+  let hostTxt = '0.0.0.0'
+  let portTxt = 6379
 
-  for await (const { host, port } of redis) {
+  for await (const { host, port, uri, username, password } of redis) {
     const requestResponse = await redisRequest({
-      host: host,
-      port: port,
+      host,
+      port,
+      username,
+      password,
+      uri,
     })
     const { body, responseTime, status } = requestResponse
     const isAlertTriggered = status !== 200
     const timeNow = new Date().toISOString()
-    const logMessage = `${timeNow} ${checkOrder} id:${id} redis:${host}:${port} ${responseTime}ms msg:${body}`
+
+    // parse uri for logMessage(host:port)
+    if (host && port) {
+      hostTxt = host
+      portTxt = port
+    } else if (uri) {
+      const parsed = parse(uri)
+      hostTxt = parsed.host
+      portTxt = Number(parsed.port)
+    }
+
+    const logMessage = `${timeNow} ${checkOrder} id:${id} redis:${hostTxt}:${portTxt} ${responseTime}ms msg:${body}`
 
     probeResults.push({ isAlertTriggered, logMessage, requestResponse })
   }
--- src/components/probe/prober/redis/request.ts
+++ src/components/probe/prober/redis/request.ts
@@ -25,14 +25,7 @@
 import { createClient } from 'redis'
 import type { ProbeRequestResponse } from '../../../../interfaces/request'
 import { differenceInMilliseconds } from 'date-fns'
-
-type RedisRequest = {
-  host: string // Host address of the redis-server
-  port: number // Port number of the redis-server
-  password?: string // Password string if AUTH is used, optional
-  username?: string // Username stgring if used, optional
-  command?: string
-}
+import type { Redis } from '../../../../interfaces/probe'
 
 type RedisResult = {
   isAlive: boolean // If redis responds to PING/commands
@@ -46,7 +39,7 @@ type RedisResult = {
  * @returns {object} ProbeRequestResponse type mapped from RedisResult
  **/
 export async function redisRequest(
-  params: RedisRequest
+  params: Redis
 ): Promise<ProbeRequestResponse> {
   const baseResponse: ProbeRequestResponse = {
     requestType: 'redis',
@@ -82,8 +75,8 @@ const CONNECTTIMEOUTMS = 10_000
  * @param {object} params is a RedisRequest type
  * @returns {object} RedisResult type contain client response
  */
-async function sendRedisRequest(params: RedisRequest): Promise<RedisResult> {
-  const { host, port, username, password } = params
+async function sendRedisRequest(params: Redis): Promise<RedisResult> {
+  const { host, port, username, password, uri } = params
 
   const result: RedisResult = {
     isAlive: false,
@@ -91,15 +84,18 @@ async function sendRedisRequest(params: RedisRequest): Promise<RedisResult> {
   }
 
   try {
-    const client = createClient({
-      socket: {
-        host: host,
-        port: port,
-        connectTimeout: CONNECTTIMEOUTMS,
-      },
-      password: password,
-      username: username,
-    })
+    const client =
+      host && port
+        ? createClient({
+            socket: {
+              host: host,
+              port: port,
+              connectTimeout: CONNECTTIMEOUTMS,
+            },
+            password: password,
+            username: username,
+          })
+        : createClient({ url: uri })
 
     await client.connect()
 
