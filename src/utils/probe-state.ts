d2552de1b234fc6d143808ff32be7f2b300e9db4
haricnugraha <haricnugraha@gmail.com>
2023-06-21 13:04:20 +0700

    refactor: extract getLastStart function

--- src/utils/probe-state.ts
+++ src/utils/probe-state.ts
@@ -1,3 +1,27 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2021 Hyperjump Technology                                        *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
 import { assign, createMachine, EventObject, interpret } from 'xstate'
 
 import { Probe } from '../interfaces/probe'
@@ -52,11 +76,13 @@ const probeInterpreters = new Map()
 export function initializeProbeStates(probes: Probe[]): void {
   probeInterpreters.clear()
 
-  for (const probe of probes) {
-    const createdAt = probe.lastEvent?.createdAt
+  const getLastStart = (probe: Probe) => {
+    return probe.lastEvent?.createdAt
       ? new Date(probe.lastEvent.createdAt)
       : new Date()
+  }
 
+  for (const probe of probes) {
     const recoveredAt = probe.lastEvent?.recoveredAt
       ? new Date(probe.lastEvent.recoveredAt)
       : new Date()
@@ -64,8 +90,10 @@ export function initializeProbeStates(probes: Probe[]): void {
     const interpreter = interpret(
       probeStateMachine.withContext({
         cycle: 0,
-        lastStart: createdAt,
-        lastFinish: probe.lastEvent?.recoveredAt ? recoveredAt : createdAt,
+        lastStart: getLastStart(probe),
+        lastFinish: probe.lastEvent?.recoveredAt
+          ? recoveredAt
+          : getLastStart(probe),
       })
     ).start()
 
