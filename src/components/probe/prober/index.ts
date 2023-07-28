8878bde1e424ec1d0ff59b1e4bc75db16cde962a
haricnugraha <haricnugraha@gmail.com>
2023-07-28 12:57:10 +0700

    feat: implement verboseStartupMessage for each prober

--- src/components/probe/prober/index.ts
+++ src/components/probe/prober/index.ts
@@ -1,3 +1,27 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2021 Hyperjump Technology                                        *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
 import type { Notification } from '@hyperjumptech/monika-notification'
 import { checkThresholdsAndSendAlert } from '..'
 import { getContext } from '../../../context'
@@ -54,11 +78,11 @@ export class BaseProber implements Prober {
     const { description, id, interval, name } = this.probeConfig
 
     let result = `- Probe ID: ${id}
-Name: ${name}
-Description: ${description || '-'}
-Interval: ${interval}
+  Name: ${name}
+  Description: ${description || '-'}
+  Interval: ${interval}
 `
-    result += `    Requests:\n`
+    result += '  Requests:\n'
     result += this.generateProbeRequestMessage()
     result += this.generateAlertMessage()
 
@@ -71,11 +95,11 @@ Interval: ${interval}
     for (const request of this.probeConfig.requests || []) {
       const { body, headers, method, url } = request
 
-      startupMessage += `      - Request Method: ${method || `GET`}
+      startupMessage += `  - Request Method: ${method || `GET`}
     Request URL: ${url}
     Request Headers: ${JSON.stringify(headers) || `-`}
     Request Body: ${JSON.stringify(body) || `-`}
-  `
+`
     }
 
     return startupMessage
