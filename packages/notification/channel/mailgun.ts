f7a5c67fa69690a9be733256b252ab5d71468726
Raosan Fikri <raosan@hyperjump.tech>
2023-08-22 14:43:28 +0700

    reorder

--- packages/notification/channel/mailgun.ts
+++ packages/notification/channel/mailgun.ts
@@ -78,6 +78,33 @@ export const send = async (
   })
 }
 
+export const sendWithCustomContent = async (
+  { apiKey, domain, recipients, username = 'api' }: NotificationData,
+  content: Content
+): Promise<void> => {
+  const url = `https://api.mailgun.net/v3/${domain}/messages`
+  const auth = Buffer.from(username + ':' + apiKey).toString('base64')
+  const data = content
+
+  const formData = new FormData()
+  formData.append('from', data.from)
+  formData.append('subject', data.subject)
+  formData.append('html', data.html)
+  formData.append('text', data.text)
+  for (const email of recipients) formData.append('to', email)
+  if (data.headerReference)
+    formData.append('h:References', data.headerReference)
+
+  await sendHttpRequest({
+    method: 'POST',
+    url,
+    headers: {
+      Authorization: 'Basic ' + auth,
+    },
+    data: formData,
+  })
+}
+
 export function getContent({
   body,
   subject,
@@ -113,33 +140,6 @@ export function getContent({
     html,
     text,
   }
-}
-
-export const sendWithCustomContent = async (
-  { apiKey, domain, recipients, username = 'api' }: NotificationData,
-  content: Content
-): Promise<void> => {
-  const url = `https://api.mailgun.net/v3/${domain}/messages`
-  const auth = Buffer.from(username + ':' + apiKey).toString('base64')
-  const data = content
-
-  const formData = new FormData()
-  formData.append('from', data.from)
-  formData.append('subject', data.subject)
-  formData.append('html', data.html)
-  formData.append('text', data.text)
-  for (const email of recipients) formData.append('to', email)
-  if (data.headerReference)
-    formData.append('h:References', data.headerReference)
-
-  await sendHttpRequest({
-    method: 'POST',
-    url,
-    headers: {
-      Authorization: 'Basic ' + auth,
-    },
-    data: formData,
-  })
 }
 
 export function additionalStartupMessage({
