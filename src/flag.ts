9bf3288956dee161072f94deea739952fe46785c
Hari Nugraha <15191978+haricnugraha@users.noreply.github.com>
2023-10-16 15:38:29 +0700

    Fix: Fail to Connect to Symon (#1144)
    * fix: unassign enum value
    
    * test: skip blocking tests
    
    * fix: cannot find report file
    
    ---------
    
    Co-authored-by: Nico Prananta <58848001+nico-hyperjump@users.noreply.github.com>

--- src/flag.ts
+++ src/flag.ts
@@ -1,13 +1,37 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright (c) 2021 Hyperjump Technology                                        *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
 import { Flags } from '@oclif/core'
 
 export enum SYMON_API_VERSION {
-  'v1',
-  'v2',
+  'v1' = 'v1',
+  'v2' = 'v2',
 }
 
 export const symonAPIVersion = Flags.custom<SYMON_API_VERSION>({
   default: SYMON_API_VERSION.v1,
-  options: ['v1', 'v2'],
+  options: [SYMON_API_VERSION.v1, SYMON_API_VERSION.v2],
   description:
     'Symon API version to use. Available options: v1, v2. Default: v1',
 })
